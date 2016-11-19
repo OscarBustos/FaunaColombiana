cf246983d9e896c91d4e0bab4ad0a86b896dd875
Oscar Bustos <oscaribh2011@gmail.com>
2016-11-18 21:07:16 -0500

    se agrega escenario1 donde se agregan los carteles con los scripts de carga dinamica de imagenes y textos.

---
+++ Assets/LoadImages.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadImages : MonoBehaviour {
+	string url ="images/Koala";
+	Texture2D img;
+	// Use this for initialization
+	void Start () {
+		GameObject myPlane = GameObject.Find ("imagen_1");
+		Texture2D img = (Texture2D)Resources.Load (url);
+		myPlane.GetComponent<Renderer> ().material.mainTexture = img;
+		url = "prueba";
+		GameObject myPanel = GameObject.Find ("titulo_pergamino_1");
+		TextAsset imgPergamino = (TextAsset)Resources.Load (url);
+		myPanel.GetComponent<Text>().text = imgPergamino.text;
+	}
+		//StartCoroutine(LoadImage());
+
+}
