a963c5f740990ccd425c6e425ec70a1fdab1b03a
Oscar Bustos <oscaribh2011@gmail.com>
2016-11-26 12:15:35 -0500

    se agregan mas letreros, y se amplia el ambiente

--- Assets/LoadImages.cs
+++ Assets/LoadImages.cs
@@ -3,17 +3,44 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class LoadImages : MonoBehaviour {
-	string url ="images/Koala";
+	public const  int numeroAnimales = 9;
+	string url ="images/";
 	Texture2D img;
 	// Use this for initialization
 	void Start () {
-		GameObject myPlane = GameObject.Find ("imagen_1");
+	/*	
+	  GameObject myPlane = GameObject.Find ("imagen_1");
 		Texture2D img = (Texture2D)Resources.Load (url);
 		myPlane.GetComponent<Renderer> ().material.mainTexture = img;
 		url = "prueba";
 		GameObject myPanel = GameObject.Find ("titulo_pergamino_1");
 		TextAsset imgPergamino = (TextAsset)Resources.Load (url);
 		myPanel.GetComponent<Text>().text = imgPergamino.text;
+*/
+		string region = "region_1/";
+
+		for (int i = 1; i <= numeroAnimales; i++) {
+			// consultar y agregar las imagenes de los animales 
+			string animal = "animal_" + i;
+			string urlImagenAnimal = url + region + animal+"/imagen_"+i;
+			GameObject imagePlane = GameObject.Find ("imagen_" + i);
+			Texture2D img = (Texture2D)Resources.Load (urlImagenAnimal);
+			imagePlane.GetComponent<Renderer> ().material.mainTexture = img;
+
+			// Consultar y agregar el titulo (nombre) del animal
+			string nombreAnimal ="titulo_" + i;
+			string urlNombreAnimal = url + region + animal + "/" + nombreAnimal;
+			GameObject panelTitulo = GameObject.Find ("titulo_pergamino_" + i);
+			TextAsset tituloPergamino = (TextAsset)Resources.Load (urlNombreAnimal);
+			panelTitulo.GetComponent<Text>().text = tituloPergamino.text;
+
+			// Consultar y agregar la descripcion  del animal
+			string descripcionAnimal ="descripcion_" + i;
+			string urlDescripcionAnimal = url + region + animal + "/" + descripcionAnimal;
+			GameObject panelDescripcion = GameObject.Find ("descripcion_pergamino_" + i);
+			TextAsset descripcionPergamino = (TextAsset)Resources.Load (urlDescripcionAnimal);
+			panelDescripcion.GetComponent<Text>().text = descripcionPergamino.text;
+		}
 	}
 		//StartCoroutine(LoadImage());
 
---
+++ Assets/PauseManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour {
+
+	public GameObject[] pauseObjects;
+
+	void Start () {
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (Time.timeScale == 1) {
+				Time.timeScale = 0;
+				showPaused ();
+			} else if (Time.timeScale == 0) {
+				Time.timeScale = 1;
+				hidePaused();
+			}				
+		}
+	}
+
+	public void pauseControl(){
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+			showPaused ();
+		} else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+			hidePaused ();
+		}
+	}
+
+	public void showPaused(){
+		foreach (GameObject g in pauseObjects) {
+			g.SetActive (true);
+		}
+	}
+
+	public void hidePaused(){
+		foreach (GameObject g in pauseObjects) {
+			g.SetActive (false);
+		}
+	}
+
+	public void LoadLevel(int scene){
+		SceneManager.LoadScene (scene);
+	}
+
+	public void quit(){
+		Application.Quit();
+	}
+}
